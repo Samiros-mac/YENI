10011e67f8eb059b7baa38e51f9dcf7cce443c92
Samir Babayev <sb829009@gmail.com>
2022-05-06 19:05:57 -0400

    3rd adding

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1*num2;
     }
+    //3rd adding
         
     public static double divide(double n1, double n2){
         double result = n1/n2;
