1c5facdc3cddd6010144a5fa46323451d21c7aaf
Samiros-mac <54758084+Samiros-mac@users.noreply.github.com>
2022-05-06 20:15:23 -0400

    Update Calculator.java
    6th adding

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,6 +15,7 @@ public class Calculator {
     //3rd adding
     //4rd adding
     //5th adding
+    //6th adding
         
     public static double divide(double n1, double n2){
         double result = n1/n2;
